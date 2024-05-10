5f62fe6236ed0d812f21009bd9e8f57f40b1693e
HarithZulhairi <misterwhoopy@gmail.com>
2024-05-10 13:06:55 +0800

    Update PlatinumUploadPublication.blade.php

--- resources/views/manage_publication/PlatinumUploadPublication.blade.php
+++ resources/views/manage_publication/PlatinumUploadPublication.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>hi</p>
-</body>
-</html>
+@extends('layouts/master')
+@section('content')
+
+<div>
+    <h2>upload</h2>
+</div>
+
+@endsection
