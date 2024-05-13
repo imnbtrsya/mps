dd3461c6e70eb2e81d5e67a01967f8be5749969e
imanbatrisya <165252961+imanbatrisya@users.noreply.github.com>
2024-05-13 15:27:25 +0800

    model & controller

---
+++ app/Http/Controllers/ExpertController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExpertDomain extends Controller
+{
+    //
+}
---
+++ app/Models/ExpertDomain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpertDomain extends Model
+{
+    use HasFactory;
+
+    protected $table = '_expert__domain';
+
+    protected $primaryKey = 'E_ID';
+
+
+    protected $fillable = [
+        'E_Name',
+        'E_Title',
+        'E_Position',
+        'E_Workplace',
+        'E_Qualification',
+        'E_Email',
+        'E_Field',
+        'E_Research',
+        'E_Publications'
+    ];
+}
