943353ad1d1a14dca64d662d945343e94e31376f
HarithZulhairi <misterwhoopy@gmail.com>
2024-05-26 14:36:59 +0800

    sds

--- resources/views/manage_publication/MentorListPublication.blade.php
+++ resources/views/manage_publication/MentorListPublication.blade.php
@@ -18,28 +18,26 @@
     <table>
         <tr>
             <th class="mypub-th" style="width: 10%;">No.</th>
-            <th class="mypub-th" style="width: 50%;">Publication title</th>
-            <th class="mypub-th" style="width: 50%;">Authors</th>
-            <th class="mypub-th" style="width: 50%;">Expert</th>
-            <th class="mypub-th" style="width: 50%;">Publication type</th>
-            <th class="mypub-th" style="width: 50%;">Action</th>
+            <th class="mypub-th" style="width: 30%;">Publication title</th>
+            <th class="mypub-th" style="width: 20%;">Authors</th>
+            <th class="mypub-th" style="width: 20%;">Expert</th>
+            <th class="mypub-th" style="width: 20%;">Publication type</th>
+            <th class="mypub-th" style="width: 30%;">Action</th>
         </tr>
         @foreach($publications as $publication)
             <tr>
                 <td>{{ $loop->iteration }}</td>
                 <td>{{$publication->Pb_title}}</td>
+                <td>{{$publication->Pb_authors}}</td>
+                <td>{{$publication->Pb_belongs}}</td>
+                <td>{{$publication->Pb_type}}</td>
                 <td class="action-buttons-container">
                     <a href="{{ route('manage_publication.PlatinumViewPublication', ['publication' => $publication->Pb_ID]) }}">
                         <button class="action-button">View</button>
                     </a>
                     <a href="{{ route('manage_publication.PlatinumEditPublication', ['publication' => $publication->Pb_ID]) }}">
-                        <button class="action-button">Edit</button>
+                        <button class="action-button">Generate</button>
                     </a>
-                    <form method="post" action="{{ route('manage_publication.delete', ['publication' => $publication]) }}" onsubmit="showPopup()">
-                        @csrf
-                        @method('delete')
-                        <input type="submit" class="action-button" value="Delete">
-                    </form>
                 </td>
             </tr>
         @endforeach
