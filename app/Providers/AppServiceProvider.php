b4295898b1024b50d63ceb728534605e60248af1
Iman Batrisya (Vintedge) <iman@vintedge.com>
2026-08-11 11:39:19 +0800

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
+
+        if (app()->environment('production')) {
+            \URL::forceScheme('https');
     }
 }
